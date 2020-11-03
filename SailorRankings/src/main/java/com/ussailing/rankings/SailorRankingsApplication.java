b618655c288824e6feee8077f00667c950414886
Dan Birmingham <dgbirm@gmail.com>
2020-11-02 21:23:57 -0500

    [spring] create framework

---
+++ SailorRankings/src/main/java/com/ussailing/rankings/SailorRankingsApplication.java
@@ -0,0 +1,13 @@
+package com.ussailing.rankings;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SailorRankingsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SailorRankingsApplication.class, args);
+	}
+
+}
---
+++ SailorRankings/src/test/java/com/ussailing/rankings/SailorRankingsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ussailing.rankings;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SailorRankingsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
